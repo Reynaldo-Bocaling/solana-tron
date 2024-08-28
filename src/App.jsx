72ab0c527825d6829aeb62d7667de0ec0ea10e02
Reynaldo <reynaldobocaling@gmail.com>
2024-08-28 20:35:46 +0800

    adjust size

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
 
   return (
     <div className="relative flex flex-col md:flex-row h-screen md:h-screen overflow-hidden bg-black">
-      <div className="w-full  h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-7 pt-9 md:pb-0 md:pt-12 border-b-2 md:border-none border-sky-500">
+      <div className="w-full  h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-7 pt-4 md:pb-0 md:pt-12 border-b-2 md:border-none border-sky-500">
         <Socials based="Tron" />
         <img
           src="/img/tron_logo.png"
@@ -54,7 +54,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className="w-full h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-4 pt-9 md:pb-0 md:pt-12 border-b-2 md:border-none border-sky-500">
+      <div className="w-full h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-4 pt-4 md:pb-0 md:pt-12 border-b-2 md:border-none border-sky-500">
         <Socials based="Solana" />
         <img
           src="/img/solana_logo.png"
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -12,7 +12,7 @@ const Socials = ({ based }) => {
       <p className=" text-white text-lg md:text-2xl tracking-wider font-semibold  bg-[rgba(58,57,57,0.7)] py-3 px-9 rounded-full z-[99]">
         {based}
       </p>
-      <div className="relative flex items-center justify-center gap-5 mt-4 z-50 mb-2">
+      <div className="relative flex items-center justify-center gap-5 mt-4 z-50">
         {data.map((item, index) => (
           <a key={index} href={item.url} className="cursor-pointer z-50">
             <img src={item.img} className="w-14 h-14 " alt="" />
