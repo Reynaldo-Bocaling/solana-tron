f5bb6d995bbd0c61878126d2fce124b54e8c125a
Reynaldo <reynaldobocaling@gmail.com>
2024-08-28 21:19:35 +0800

    adjust size

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
 
   return (
     <div className="relative flex flex-col md:flex-row h-screen md:h-screen overflow-hidden bg-black">
-      <div className="w-full  h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-7 pt-7 md:pb-0 md:pt-12 shadow-md md:shadow-none shadow-red-500">
+      <div className="w-full  h-[39%] md:w-1/2 md:h-full relative flex flex-col items-center justify-start gap-3 md:gap-7 pb-7 pt-7 md:pb-0 md:pt-12 ">
         <Socials based="Tron" />
         <img
           src="/img/tron_logo.png"
