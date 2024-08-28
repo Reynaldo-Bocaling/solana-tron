9b7be95db2e2b6767765ac527010b3c704db25d2
Reynaldo <reynaldobocaling@gmail.com>
2024-08-28 20:23:59 +0800

    upload

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -12,10 +12,10 @@ const Socials = ({ based }) => {
       <p className=" text-white text-lg md:text-2xl tracking-wider font-semibold  bg-[rgba(58,57,57,0.7)] py-3 px-9 rounded-full z-[99]">
         {based}
       </p>
-      <div className="relative flex items-center justify-center gap-7 mt-4 z-50 mb-2">
+      <div className="relative flex items-center justify-center gap-5 mt-4 z-50 mb-2">
         {data.map((item, index) => (
           <a key={index} href={item.url} className="cursor-pointer z-50">
-            <img src={item.img} className="w-16 h-16 " alt="" />
+            <img src={item.img} className="w-14 h-14 " alt="" />
           </a>
         ))}
       </div>
